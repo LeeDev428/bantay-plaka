c51c1d9c83ff679a1f593a08c6337554cef21eba
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-11 02:46:03 +0800

    Update urls.py

--- apps/logs/urls.py
+++ apps/logs/urls.py
@@ -4,4 +4,6 @@ from apps.logs import views
 urlpatterns = [
     path('manual/', views.manual_entry, name='manual_entry'),
     path('', views.log_list, name='log_list'),
+    path('<int:pk>/edit/', views.log_edit, name='log_edit'),
+    path('<int:pk>/delete/', views.log_delete, name='log_delete'),
 ]

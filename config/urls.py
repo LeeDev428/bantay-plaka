9a0daa75cec300408e7c192e8094d3993c355788
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-13 02:37:24 +0800

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +13,5 @@ urlpatterns = [
     path('logs/', include('apps.logs.urls')),
     path('detection/', include('apps.detection.urls')),
     path('reports/', include('apps.reports.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + staticfiles_urlpatterns()

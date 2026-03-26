09eb82e0c57030925857fdc88bf19caaa76d8f36
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-27 00:53:57 +0800

    Update urls.py

--- apps/visitors/urls.py
+++ apps/visitors/urls.py
@@ -4,4 +4,6 @@ from apps.visitors import views
 urlpatterns = [
     path('', views.visitor_list, name='visitor_list'),
     path('log/', views.visitor_log_entry, name='visitor_log_entry'),
+    path('blacklist/', views.blacklist_list, name='blacklist_list'),
+    path('blacklist/<int:pk>/toggle/', views.blacklist_toggle, name='blacklist_toggle'),
 ]

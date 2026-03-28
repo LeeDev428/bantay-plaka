aa994cae1b77bcff848170ff89d27c41cf402442
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-28 16:23:50 +0800

    Update urls.py

--- apps/detection/urls.py
+++ apps/detection/urls.py
@@ -4,4 +4,5 @@ from apps.detection import views
 urlpatterns = [
     path('ingest/', views.ingest_plate, name='ingest_plate'),
     path('preview/<str:camera_role>/', views.camera_preview, name='camera_preview'),
+    path('frame/<str:camera_role>/', views.camera_frame, name='camera_frame'),
 ]

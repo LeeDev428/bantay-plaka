3d85992e46ecac53f85c65a1fc3add7fe501285b
Lee Torres <grafrafraftorres28@gmail.com>
2026-04-09 18:43:43 +0800

    Update urls.py

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from apps.accounts import views
 
 urlpatterns = [
     path('', views.dashboard_redirect, name='home'),
     path('login/', views.BantayPlakaLoginView.as_view(), name='login'),
     path('resident/register/', views.resident_register, name='resident_register'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
     path('logout/', views.BantayPlakaLogoutView.as_view(), name='logout'),
 ]

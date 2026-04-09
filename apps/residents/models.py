4f7b96fb3cfa7ca57627acc1e87a2f561fa30055
Lee Torres <grafrafraftorres28@gmail.com>
2026-04-09 16:00:27 +0800

    Update models.py

--- apps/residents/models.py
+++ apps/residents/models.py
@@ -3,10 +3,44 @@ from apps.accounts.models import User
 
 
 class Resident(models.Model):
+    SEX_MALE = 'MALE'
+    SEX_FEMALE = 'FEMALE'
+    SEX_OTHER = 'OTHER'
+    SEX_CHOICES = [
+        (SEX_MALE, 'Male'),
+        (SEX_FEMALE, 'Female'),
+        (SEX_OTHER, 'Other'),
+    ]
+
     first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100)
+    suffix = models.CharField(max_length=20, blank=True)
+    sex = models.CharField(max_length=10, choices=SEX_CHOICES, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    age = models.PositiveSmallIntegerField(null=True, blank=True)
     address = models.CharField(max_length=255)
+    street_number = models.CharField(max_length=50, blank=True)
+    street_name = models.CharField(max_length=150, blank=True)
     contact_number = models.CharField(max_length=20, blank=True)
+    valid_id_type = models.CharField(max_length=80, blank=True)
+    valid_id_image = models.ImageField(upload_to='resident_ids/', null=True, blank=True)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='resident_profile',
+    )
+    is_approved = models.BooleanField(default=True, db_index=True)
+    approved_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='approved_residents',
+    )
+    approved_at = models.DateTimeField(null=True, blank=True)
     registered_by = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True, related_name='registered_residents'
     )
@@ -19,7 +53,9 @@ class Resident(models.Model):
 
     @property
     def full_name(self):
-        return f'{self.first_name} {self.last_name}'
+        middle = f' {self.middle_name}' if self.middle_name else ''
+        suffix = f' {self.suffix}' if self.suffix else ''
+        return f'{self.first_name}{middle} {self.last_name}{suffix}'.strip()
 
     def __str__(self):
         return self.full_name

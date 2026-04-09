35bcb0e3d47bda82f9538edd7d1511ed7305d6bf
Lee Torres <grafrafraftorres28@gmail.com>
2026-04-09 16:00:25 +0800

    Update forms.py

--- apps/residents/forms.py
+++ apps/residents/forms.py
@@ -1,17 +1,52 @@
 from django import forms
+from django.utils import timezone
+
 from apps.residents.models import Resident, Vehicle
 
 
 class ResidentForm(forms.ModelForm):
     class Meta:
         model = Resident
-        fields = ['first_name', 'last_name', 'address', 'contact_number']
+        fields = [
+            'first_name',
+            'middle_name',
+            'last_name',
+            'suffix',
+            'sex',
+            'birth_date',
+            'age',
+            'contact_number',
+            'street_number',
+            'street_name',
+            'address',
+            'valid_id_type',
+            'valid_id_image',
+        ]
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
+            'middle_name': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
             'last_name': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
+            'suffix': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
+            'sex': forms.Select(attrs={'class': 'select select-bordered w-full'}),
+            'birth_date': forms.DateInput(attrs={'class': 'input input-bordered w-full', 'type': 'date'}),
+            'age': forms.NumberInput(attrs={'class': 'input input-bordered w-full', 'min': 0}),
             'address': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
+            'street_number': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
+            'street_name': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
             'contact_number': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
+            'valid_id_type': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
+            'valid_id_image': forms.ClearableFileInput(attrs={'class': 'file-input file-input-bordered w-full'}),
         }
+
+    def clean(self):
+        cleaned = super().clean()
+        birth_date = cleaned.get('birth_date')
+        if birth_date:
+            today = timezone.localdate()
+            age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+            if age >= 0:
+                cleaned['age'] = age
+        return cleaned
 
 
 class VehicleForm(forms.ModelForm):

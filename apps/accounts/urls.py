72eff8150f5c16d65628cd7717d129127b748c2c
Lee Torres <grafrafraftorres28@gmail.com>
2026-04-09 16:02:58 +0800

    Update urls.py

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,5 +4,6 @@ from apps.accounts import views
 urlpatterns = [
     path('', views.dashboard_redirect, name='home'),
     path('login/', views.BantayPlakaLoginView.as_view(), name='login'),
+    path('resident/register/', views.resident_register, name='resident_register'),
     path('logout/', views.BantayPlakaLogoutView.as_view(), name='logout'),
 ]

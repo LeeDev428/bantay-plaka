67c189bc8a4f69d86f262f299574971b7e469373
Lee Torres <grafrafraftorres28@gmail.com>
2026-04-09 18:45:34 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -102,6 +102,14 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/login/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = env.int('EMAIL_PORT', default=587)
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='grafrafraftorres28@gmail.com')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='czar eklw lofi lann')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
+
 # API key used by the ANPR engine script to authenticate POSTs to /detection/ingest/
 # Set this in .env as ANPR_API_KEY=<your-secret-key>
 ANPR_API_KEY = env('ANPR_API_KEY', default='')

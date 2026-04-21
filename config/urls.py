88e7564c0f079e326eb42aace893f0de664d5759
Lee Torres <grafrafraftorres28@gmail.com>
2026-04-22 04:30:00 +0800

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.http import HttpResponse
+from django.views.static import serve as static_serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,5 +16,6 @@ urlpatterns = [
     path('logs/', include('apps.logs.urls')),
     path('detection/', include('apps.detection.urls')),
     path('reports/', include('apps.reports.urls')),
+    re_path(r'^media/(?P<path>.*)$', static_serve, {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
   + staticfiles_urlpatterns()

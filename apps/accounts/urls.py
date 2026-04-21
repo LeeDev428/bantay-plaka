45816747dacfebae85b389aef0e2d9a1cd51ee18
Lee Torres <grafrafraftorres28@gmail.com>
2026-04-22 04:39:48 +0800

    Update urls.py

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.dashboard_redirect, name='home'),
     path('login/', views.BantayPlakaLoginView.as_view(), name='login'),
     path('resident/register/', views.resident_register, name='resident_register'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),

808a3d33c7c90771e5f7bb15d2b12eb8719bf364
Lee Torres <grafrafraftorres28@gmail.com>
2026-04-21 16:56:27 +0800

    Update models.py

--- apps/residents/models.py
+++ apps/residents/models.py
@@ -11,6 +11,29 @@ class Resident(models.Model):
         (SEX_FEMALE, 'Female'),
         (SEX_OTHER, 'Other'),
     ]
+    VALID_ID_TYPE_CHOICES = [
+        ('', '-- Select Valid ID --'),
+        ('PHILSYS_ID', 'PhilSys National ID'),
+        ('PASSPORT', 'Passport'),
+        ('DRIVERS_LICENSE', "Driver's License"),
+        ('UMID', 'UMID'),
+        ('SSS_ID', 'SSS ID'),
+        ('GSIS_ECARD', 'GSIS eCard'),
+        ('PRC_ID', 'PRC ID'),
+        ('POSTAL_ID', 'Postal ID'),
+        ('VOTERS_ID', "Voter's ID"),
+        ('SENIOR_CITIZEN_ID', 'Senior Citizen ID'),
+        ('PWD_ID', 'PWD ID'),
+        ('TIN_ID', 'TIN ID'),
+        ('NBI_CLEARANCE', 'NBI Clearance'),
+        ('POLICE_CLEARANCE', 'Police Clearance'),
+        ('BARANGAY_ID', 'Barangay ID'),
+        ('OWWA_ID', 'OWWA ID'),
+        ('SEAMANS_BOOK', "Seaman's Book"),
+        ('SCHOOL_ID', 'School ID'),
+        ('COMPANY_ID', 'Company ID'),
+        ('OTHER_GOVERNMENT_ID', 'Other Government Issued ID'),
+    ]
 
     first_name = models.CharField(max_length=100)
     middle_name = models.CharField(max_length=100, blank=True)

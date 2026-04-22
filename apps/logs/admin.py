5cbddd64c112153df63045e6ee1931fc82a8eac0
Lee Torres <grafrafraftorres28@gmail.com>
2026-04-22 13:05:58 +0800

    Update admin.py

--- apps/logs/admin.py
+++ apps/logs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.logs.models import VehicleLog
+from apps.logs.models import VehicleLog, CameraFeedSnapshot
 
 
 @admin.register(VehicleLog)
@@ -8,3 +8,10 @@ class VehicleLogAdmin(admin.ModelAdmin):
     list_filter = ['camera_role', 'entry_type', 'status', 'source']
     search_fields = ['plate_number', 'resident_name', 'visitor_name']
     readonly_fields = ['timestamp']
+
+
+@admin.register(CameraFeedSnapshot)
+class CameraFeedSnapshotAdmin(admin.ModelAdmin):
+    list_display = ['camera_role', 'updated_at']
+    list_filter = ['camera_role']
+    readonly_fields = ['updated_at']
